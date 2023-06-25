631db7825090a20fbaf056db41ab05161abd0f41
akiraah <rah24.@gmail.com>
2023-06-25 22:20:37 +0100

    resolving static build files error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  basePath: '/nimvikfoundation'
+  basePath: "/nimvikfoundation",
 }
 
 module.exports = nextConfig
